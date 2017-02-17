61584793ab79de9078e1f8ed36076d9e5018ada2
Ariel Triana <ariel.triana88@gmail.com>
2017-02-17 16:30:25 -0500

    XS Ok

--- Shop/js/js.js
+++ Shop/js/js.js
@@ -1,5 +1,17 @@
 $().ready(function(){
-	$("#main-menu li").hover(function(){
-		$(this).toggleClass("fon");
-	});
+	//var intViewportWidth = self.innerWidth;
+	//document.getElementById("main-logo").innerHTML=intViewportWidth;
+	if (window.innerWidth < 1400) {
+		document.getElementById("search").size=20;
+	}
+	if (window.innerWidth < 768) {
+		document.getElementById("search").size=18;
+	}
+	if (window.innerWidth < 420) {
+		document.getElementById("search").size=12;
+	}
+	if (window.innerWidth < 320) {
+		document.getElementById("search").size=10;
+		//document.getElementById("menu-search").style.fontSize="8px";
+	}	
 });
