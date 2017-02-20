3b14cd53f4506ed0644090715f69f328eb0b3288
Ariel Triana <ariel.triana88@gmail.com>
2017-02-20 10:18:17 -0500

    Add CategoryProduct

--- Shop/js/js.js
+++ Shop/js/js.js
@@ -1,17 +1,19 @@
-$().ready(function(){
-	//var intViewportWidth = self.innerWidth;
-	//document.getElementById("main-logo").innerHTML=intViewportWidth;
-	if (window.innerWidth < 1400) {
-		document.getElementById("search").size=20;
-	}
-	if (window.innerWidth < 768) {
-		document.getElementById("search").size=18;
-	}
-	if (window.innerWidth < 420) {
-		document.getElementById("search").size=12;
-	}
-	if (window.innerWidth < 320) {
-		document.getElementById("search").size=10;
-		//document.getElementById("menu-search").style.fontSize="8px";
-	}	
+$().ready(function() {
+    //var intViewportWidth = self.innerWidth;
+    //document.getElementById("main-logo").innerHTML=intViewportWidth;
+    var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    //var h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+    if (w < 1400) {
+        document.getElementById("search").size = 20;
+    }
+    if (w < 768) {
+        document.getElementById("search").size = 18;
+    }
+    if (w < 420) {
+        document.getElementById("search").size = 12;
+    }
+    if (w < 320) {
+        document.getElementById("search").size = 10;
+        //document.getElementById("menu-search").style.fontSize="8px";
+    }
 });
